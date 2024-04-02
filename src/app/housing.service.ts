30b487b5f17a421096f077a678a759935e3a3642
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2024-04-02 17:42:11 -0300

    first commit

---
+++ src/app/housing.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housing-location';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  url = 'http://localhost:3000/location'
+
+  constructor() { }
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url)
+    return await data.json() ?? [];
+  }
+
+  async getHousingLocationById(id: Number): Promise<HousingLocation | undefined> {
+    console.log(id);
+
+    const data = await fetch(`${this.url}/${id}`)
+    console.log(data);
+
+    return await data.json() ?? {}
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email);
+
+  }
+}
